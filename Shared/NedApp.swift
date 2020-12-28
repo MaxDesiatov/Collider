3a454b653667b76b529f2f8e95fdd1ec1b7159e9
Max Desiatov <max@desiatov.com>
2020-12-28 19:53:32 +0000

    Initial Commit

---
+++ Shared/NedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NedApp.swift
+//  Shared
+//
+//  Created by Max Desiatov on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct NedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
